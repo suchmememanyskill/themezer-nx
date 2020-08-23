fb294f0f057a38bc6900f6860aa6a868dead70a1
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2020-08-23 20:22:07 +0200

    Change design of packdetails, update JAGL

--- themezer-nx/source/gfx/packdetails.c
+++ themezer-nx/source/gfx/packdetails.c
@@ -3,8 +3,11 @@
 ShapeLinker_t *CreatePackDetailsMenu(ShapeLinker_t *items, RequestInfo_t *rI){
     ShapeLinker_t *out = NULL;
 
-    ShapeLinkAdd(&out, ListGridCreate(POS(0, 60, SCREEN_W, SCREEN_H - 60), 4, 260, COLOR_CENTERLISTBG, COLOR_CENTERLISTSELECTION, COLOR_CENTERLISTPRESS, (items) ? GRID_NOSIDEESC : LIST_DISABLED, items, ThemeSelect, NULL, FONT_TEXT[FSize23]), ListGridType);
-    ShapeLinkAdd(&out, ButtonCreate(POS(0, 0, SCREEN_W, 60), COLOR_TOPBAR, COLOR_RED, COLOR_WHITE, COLOR_TOPBARSELECTION, 0, ButtonStyleTopStrip, "Back", FONT_TEXT[FSize30], exitFunc), ButtonType);
+    SDL_Texture *screenshot = ScreenshotToTexture();
+    ShapeLinkAdd(&out, ImageCreate(screenshot, POS(0, 0, SCREEN_W, SCREEN_H), IMAGE_CLEANUPTEX), ImageType);
+    ShapeLinkAdd(&out, RectangleCreate(POS(0, 0, SCREEN_W, SCREEN_H), COLOR(0,0,0,200), 1), RectangleType);
+    ShapeLinkAdd(&out, ListGridCreate(POS(160, 100, SCREEN_W - 320, SCREEN_H - 150), 3, 260, COLOR_CENTERLISTBG, COLOR_CENTERLISTSELECTION, COLOR_CENTERLISTPRESS, (items) ? GRID_NOSIDEESC : LIST_DISABLED, items, ThemeSelect, NULL, FONT_TEXT[FSize23]), ListGridType);
+    ShapeLinkAdd(&out, ButtonCreate(POS(160, 50, SCREEN_W - 320, 50), COLOR_TOPBAR, COLOR_RED, COLOR_WHITE, COLOR_TOPBARSELECTION, 0, ButtonStyleTopStrip, "Back", FONT_TEXT[FSize25], exitFunc), ButtonType);
     ShapeLinkAdd(&out, rI, DataType);
 
     return out;
