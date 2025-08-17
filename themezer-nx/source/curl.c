4858e3fe43748dc80f6fc0e5516e93d2fa3ba5c4
Migush <migushthe2nd@users.noreply.github.com>
2025-08-18 00:13:32 +0200

    Fix missing closing bracket

--- themezer-nx/source/curl.c
+++ themezer-nx/source/curl.c
@@ -119,7 +119,7 @@ CURL *CreateRequest(char *url, get_request_t *data){
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "themezer-nx"
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "themezer-nx");
 
         data->buffer = malloc(CHUNK_SIZE);
         data->buflen = CHUNK_SIZE;
